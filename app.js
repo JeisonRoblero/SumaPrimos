730e1fb8aaa872ebe164442c2b805bd15dc77c3d
Jeison <jeisonroblero.ga>
2023-02-17 22:46:50 -0600

    Suma de numeros primos finalizado

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-let number = document.querySelector("#number");
-let buttonCalc = document.querySelector("#button-calc");
-let resultado = document.querySelector("#resultado");
+const buttonCalc = document.querySelector("#button-calc");
+const resultadoContainer = document.querySelector("#resultado-container");
+const sinRespuesta = document.querySelector("#sin-respuesta");
 
-number.addEventListener('keydown', () => {
-    resultado.innerHTML = number.value;
+buttonCalc.addEventListener('click', () => {
+
+    const numero = document.querySelector("#number").value;
+    const resultado = document.querySelector("#resultado");
+    const sumaNumeros = document.querySelector('#suma-numeros');
+    let suma = 0;
+    let numerosSumados = "";
+    let numeroInt = parseInt(numero);
+
+    if (numeroInt < 0 || typeof numeroInt != 'number' || !Number.isInteger(numeroInt)) {
+        sinRespuesta.style.display = 'none';
+        resultadoContainer.style.display = 'block';
+        sumaNumeros.innerHTML = '';
+        resultado.innerHTML = 'El número ingresado debe ser un número entero positivo, ingresa otro número';
+    } else {     
+        for (let i=0; i<=numero; i++) {
+            if (esPrimo(i) == true) {
+                numerosSumados += i + ' + ';
+                suma += parseInt(i);
+            }
+        }
+
+        sinRespuesta.style.display = 'none';
+        resultadoContainer.style.display = 'block';
+        sumaNumeros.innerHTML = numerosSumados.substring(0,numerosSumados.length - 2);
+        resultado.innerHTML = ' = ' + suma;
+    }
+
 });
+
+function esPrimo(n) {
+    if (n == 0 || n == 1 || n == 4) return false;
+    for(let i = 2; i < n/2; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
